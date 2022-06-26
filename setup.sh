468a22e276a59aa4d7daed0c170d1bb48ccfdf5d
RaNa MazHar Naseer <103133005+mazuu06@users.noreply.github.com>
2022-06-26 22:07:51 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 sudo apt install python3-pip -y
 python3 -m pip install selenium
-chmod 777 * 
-chmod 766  modules/*
+chmod 664 output.txt
+chmod 664 geckodriver.log
+chmod 777 geckodriver
+chmod 774 darazify.py 
+chmod 744  modules/*
+
 
